e0477c954275cddac620c519dace4bf9aff060e6
zhaiguanjun <robinzhai@126.com>
2016-02-26 14:27:55 +0800

    work2 create one student

---
+++ GitDemo/GitDemo/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GitDemo
+//
+//  Created by zhaiguanjun on 16/2/26.
+//  Copyright © 2016年 翟冠军. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ GitDemo/GitDemo/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GitDemo
+//
+//  Created by zhaiguanjun on 16/2/26.
+//  Copyright © 2016年 翟冠军. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
